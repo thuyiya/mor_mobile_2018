84f4bfec40df18b2fa45cae4f337e217ce4143c3
Yohan Kulasinghe <yohankulasinghe@gmail.com>
2018-05-14 12:18:28 +0530

    Add home,profile screens / modify home screen

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { AppRegistry, YellowBox } from 'react-native';
 import { createStackNavigator } from 'react-navigation';
-import login from './src/screens/login/login';
+import home from './src/screens/home/home';
 
 YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
 
 const Application = createStackNavigator({
-  Home: { screen: login },
+  Home: { screen: home },
 }, {
   headerMode: 'none',
   navigationOptions: {
---
+++ src/screens/home/home.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import { AppRegistry, YellowBox, TouchableOpacity, Text, StyleSheet, View } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
+import login from '../login/login';
+
+YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
+
+const Application = createStackNavigator({
+  Home: { screen: login },
+}, {
+  headerMode: 'none',
+  navigationOptions: {
+    headerVisible: false,
+  },
+});
+
+export default class App extends Component {
+    loginPage = () => {
+      Application('Home');
+    }
+
+    render() {
+      return (
+
+        <View style={styles.container}>
+
+          <Text style={styles.header}>- WELCOME To TidyMaster -</Text>
+
+          <TouchableOpacity
+            style={styles.btn}
+            onPress={this.loginPage}
+          >
+            <Text>Login</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={styles.btn}
+          >
+            <Text>Signup</Text>
+          </TouchableOpacity>
+
+        </View>
+
+      );
+    }
+}
+
+const styles = StyleSheet.create({
+  btn: {
+    alignSelf: 'stretch',
+    backgroundColor: '#01c853',
+    padding: 20,
+    alignItems: 'center',
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#2896d3',
+    paddingLeft: 40,
+    paddingRight: 40,
+  },
+  header: {
+    fontSize: 24,
+    marginBottom: 60,
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+
+});
+
+AppRegistry.registerComponent('sample_project', () => App);
