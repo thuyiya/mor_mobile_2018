ca2da0bb314f60a561e0813c0f753a3ee72d032d
Yohan Kulasinghe <yohankulasinghe@gmail.com>
2018-05-14 10:20:29 +0530

    change app.js to load login.js, Create login.js, Fix navigation warning

--- App.js
+++ App.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
-
+import { AppRegistry, YellowBox } from 'react-native';
+import { createStackNavigator  } from 'react-navigation';
 import Root from './src/main';
+import login from './src/screens/login/login';
+
+YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
+
+const Application = createStackNavigator({
+  Home: { screen: login },
+}, {
+  navigationOptions: {
+    header: false,
+  },
+});
 
 export default class App extends Component {
   render() {
-    return <Root />;
+    return (
+      <Application />
+    );
   }
 }
+
+AppRegistry.registerComponent('sample_project', () => App);
