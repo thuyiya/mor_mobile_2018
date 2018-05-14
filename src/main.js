63fc34c7be6b2fb8a7cb544a8f900f9bd23fe14f
thuyiya <thusitha.4t@gmail.com>
2018-05-14 08:04:36 +0530

    update folder structre

--- src/main.js
+++ src/main.js
@@ -1,37 +1,8 @@
 import React, { Component } from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
+import AppContainer from './app-container';
 
 export default class Root extends Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to Sample Project!
-        </Text>
-      </View>
-    );
+    return (<AppContainer />);
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
